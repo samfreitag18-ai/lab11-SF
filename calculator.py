3f08eb82343299c74ce3af2778e09481e06ec868
freitagsam <samfreitag11@gmail.com>
2026-04-13 23:28:20 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,39 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+
+def exponent(a, b):
+    return a ** b
